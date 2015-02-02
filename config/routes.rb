af7752250a8aa2e04e9aaf00042a22219e0aed4f
Esther Zinn <Bionicfemme@me.com>
2015-02-02 09:32:31 -0500

    Configured Bloccit to work in Production environment

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   get 'topics/edit'
 
   devise_for :users
+     resources :users, only: [:update]
      resources :topics do
      resources :posts, except: [:index]
+
    end
    
   get 'about' => 'welcome#about'
