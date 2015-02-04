256e6bcf8b122b14b4b3e565d77cb52c4600352f
Esther Zinn <Bionicfemme@me.com>
2015-02-03 22:32:52 -0500

    Added commenting

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202155951) do
+ActiveRecord::Schema.define(version: 20150204032219) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
